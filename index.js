3565dfff2671965b745a9c2fae415a2b2bdfa5f6
Ammar Yasser <ammaryasser.eng@gmail.com>
2024-11-28 01:53:59 +0200

    error handling
    added a group of try-catch statements

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ client.on('ready', (c) => {
 
 client.on('messageCreate', async (message) => {
     if (message.content === "ping") {
-        message.reply('node.js server is up and running');
+        try {
+            message.reply('node.js server is up and running');
+        } catch (error) {
+            console.log("ERROR at message contents! \n", error)
+        }
     }
 })
 
@@ -28,68 +32,82 @@ client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === 'help') {
+        try {
+            interaction.reply({
+                content: 'Typing the list of commands I can make \n' +
+                    '---------------------------------------------------------------\n' +
+                    '/help : lists the bot\'s available commands \n' +
+                    '---------------------------------------------------------------\n' +
+                    '/list : lists the server members depending on their status \n' +
+                    '   --> options : - \n' +
+                    '   state (optional) : accepts 4 values online, offline, dnd and idle \n' +
+                    '       online : lists offline members \n' +
+                    '       offline : lists offline members \n' +
+                    '       dnd : lists don\'t disturb members \n' +
+                    '       idle : lists idle members \n' +
+                    '   any other option or no options at all will list all members \n' +
+                    '---------------------------------------------------------------\n' +
+                    '/chart : draw a graph showing the numbers of members by their stasus \n' +
+                    '   --> options : - \n' +
+                    '   type (required) : indicates the type of the graph, either bar or pie \n' +
+                    '       bar : the graph is drawn in a bar graph \n' +
+                    '       pie : the graph is drawn in a pie graph'
+            });
+        } catch (error) {
+            console.log("ERROR at command interactions! \n", error)
+        }
 
-        interaction.reply({
-            content: 'Typing the list of commands I can make \n' +
-                '---------------------------------------------------------------\n' +
-                '/help : lists the bot\'s available commands \n' +
-                '---------------------------------------------------------------\n' +
-                '/list : lists the server members depending on their status \n' +
-                '   --> options : - \n' +
-                '   state (optional) : accepts 4 values online, offline, dnd and idle \n' +
-                '       online : lists offline members \n' +
-                '       offline : lists offline members \n' +
-                '       dnd : lists don\'t disturb members \n' +
-                '       idle : lists idle members \n' +
-                '   any other option or no options at all will list all members \n' +
-                '---------------------------------------------------------------\n' +
-                '/chart : draw a graph showing the numbers of members by their stasus \n' +
-                '   --> options : - \n' +
-                '   type (required) : indicates the type of the graph, either bar or pie \n' +
-                '       bar : the graph is drawn in a bar graph \n' +
-                '       pie : the graph is drawn in a pie graph'
-        });
     }
 
     if (interaction.commandName === 'list') {
+        try {
+            let selectedOpt = interaction.options.get('state')?.value;
 
-        let selectedOpt = interaction.options.get('state')?.value;
+            const members = await interaction.guild.members.fetch();
+            let msg = '';
+            members.map((member) => {
+                if (selectedOpt == null ||
+                    selectedOpt == undefined ||
+                    (selectedOpt != 'online' &&
+                        selectedOpt != 'offline' &&
+                        selectedOpt != 'dnd' &&
+                        selectedOpt != 'idle')) {
+                    msg += `${member.user.username} : ${member.presence ? member.presence.status : 'offline'} \n`;
+                }
+                else if ((member.presence && member.presence.status === selectedOpt) ||
+                    !member.presence && selectedOpt === 'offline') {
+                    msg += `${member.user.username} : ${selectedOpt} \n`;
+                }
+            })
 
-        const members = await interaction.guild.members.fetch();
-        let msg = '';
-        members.map((member) => {
-            if (selectedOpt == null ||
-                selectedOpt == undefined ||
-                (selectedOpt != 'online' &&
-                    selectedOpt != 'offline' &&
-                    selectedOpt != 'dnd' &&
-                    selectedOpt != 'idle')) {
-                msg += `${member.user.username} : ${member.presence ? member.presence.status : 'offline'} \n`;
-            }
-            else if ((member.presence && member.presence.status === selectedOpt) ||
-                !member.presence && selectedOpt === 'offline') {
-                msg += `${member.user.username} : ${selectedOpt} \n`;
-            }
-        })
+            interaction.reply(msg);
+        } catch (error) {
+            console.log("ERROR at command interactions! \n", error)
+        }
 
-        interaction.reply(msg);
+
     }
 
     if (interaction.commandName === 'chart') {
-        if (interaction.options.get('type') === null || interaction.options.get('type') === undefined) {
-            interaction.reply("ERROR! chart type is required! ");
+        try {
+            if (interaction.options.get('type') === null || interaction.options.get('type') === undefined) {
+                interaction.reply("ERROR! chart type is required! ");
+            }
+            else {
+                const members = await interaction.guild.members.fetch();
+                console.log(interaction.options.get('type').value);
+                let filePath = "";
+                if (interaction.options.get('type').value == 'bar')
+                    filePath = await createBarChart(members);
+                else
+                    filePath = await drawPieChart(members);
+                const attachment = new AttachmentBuilder(filePath);
+                interaction.reply({ content: 'Here is your file:', files: [attachment] });
+            }
+        } catch (error) {
+            console.log("ERROR at command interactions! \n", error)
         }
-        else {
-            const members = await interaction.guild.members.fetch();
-            console.log(interaction.options.get('type').value);
-            let filePath = "";
-            if (interaction.options.get('type').value == 'bar')
-                filePath = await createBarChart(members);
-            else
-                filePath = await drawPieChart(members);
-            const attachment = new AttachmentBuilder(filePath);
-            interaction.reply({ content: 'Here is your file:', files: [attachment] });
-        }
+
     }
 });
 
